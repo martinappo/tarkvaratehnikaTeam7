079b0194977ada7795af704f6d43e669be3981e2
martinappo <martinappo@gmail.com>
2014-10-11 17:51:13 +0300

    Build revision number with ant. Added log4j, jgoodies libraries.

--- tarkvaratehnika/src/ee/ut/math/tvt/team7/Intro.java
+++ tarkvaratehnika/src/ee/ut/math/tvt/team7/Intro.java
@@ -1,9 +1,11 @@
 package ee.ut.math.tvt.team7;
-
+import org.apache.log4j.Logger;
 import java.io.IOException;
 
 public class Intro {
-
+	static Logger log = Logger.getLogger(
+            Intro.class.getName());
+	
 	public static void main(String[] args) {
 
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -16,6 +18,7 @@ public class Intro {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+                log.info("1255 5 5 ");
             }
         });
 
