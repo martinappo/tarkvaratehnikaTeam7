625cf2b99f46a0b5acde2533e80ae3784cbc615b
Handre Elias <handreelias@gmail.com>
2014-10-14 10:34:56 +0300

    Extend POS system

--- tarkvaratehnika/src/ee/ut/math/tvt/team7/Intro.java
+++ tarkvaratehnika/src/ee/ut/math/tvt/team7/Intro.java
@@ -2,24 +2,39 @@ package ee.ut.math.tvt.team7;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
+
 public class Intro {
 
-	
-	public static void main(String[] args) {
+	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
 
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			IntroUI ui = new IntroUI();
-			
-            public void run() {
-                try {
-					ui.createAndShowGUI();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-                
-            }
-        });
+	public static void main(String[] args) throws IOException {
+
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+			IntroUI introUI = new IntroUI();
+			introUI.createAndShowGUI();
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
 
 	}
 
--- tarkvaratehnika/src/ee/ut/math/tvt/team7/IntroUI.java
+++ tarkvaratehnika/src/ee/ut/math/tvt/team7/IntroUI.java
@@ -27,91 +27,86 @@ public class IntroUI {
 	String teamMembers;
 	String teamLogo;
 	String ver;
-	static Logger log = Logger.getLogger(
-            Intro.class.getName());
+	static Logger log = Logger.getLogger(Intro.class.getName());
 
 	public void createAndShowGUI() throws IOException {
 		BasicConfigurator.configure();
-		
+
 		log.info("Intro window opened");
-		
+
 		Properties application = new Properties();
 		Properties version = new Properties();
-    	InputStream input = null;
-    	InputStream input2 = null;
-    	 
- 
-    	try {
-    		String filename2 = "version.properties";
-    		String filename = "application.properties";
-    		input = new FileInputStream(filename);
-    		input2 = new FileInputStream(filename2);
-    		
- 
-    		//load a properties file from class path, inside static method
-    		application.load(input);
-    		version.load(input2);
- 
-                //get the property value and print it out
-    		teamName = application.getProperty("teamName");
-    		teamLeader = application.getProperty("teamLeader");
-    		leaderEmail = application.getProperty("leaderEmail");
-    		teamMembers = application.getProperty("teamMembers");
-    		teamLogo= application.getProperty("teamLogo");
-    		ver = version.getProperty("build.number");
- 
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-        } finally{
-        	if(input!=null){
-        		try {
-				input.close();
-			} 
-        		catch (IOException e) {
-				e.printStackTrace();
-			}
-        	}
-        	if(input2 !=null){
-        		try {
-				input2.close();
-			} 
-        		catch (IOException e) {
-				e.printStackTrace();
-			}
-        	}
-        }
-		
-        JLabel label1 = new JLabel("Team name: "+ teamName);
-        JLabel label2 = new JLabel("Leader: " + teamLeader);
-        JLabel label3 = new JLabel("Leader email: " + leaderEmail);
-        JLabel label4 = new JLabel("Team members: " + teamMembers);
-        JLabel label5 = new JLabel("Version number: " + ver);
-        JLabel label6 = new JLabel("Logo: ");
-        
-        BufferedImage myPicture = ImageIO.read(new File(teamLogo));
-        JLabel picLabel = new JLabel(new ImageIcon(myPicture), SwingConstants.LEFT);
-       
+		InputStream input = null;
+		InputStream input2 = null;
 
-        JPanel pane = new JPanel();
-        pane.setBorder(BorderFactory.createEmptyBorder(30, //top
-            30, //left
-            10, //bottom
-            30) //right
-            );
-        pane.setLayout(new GridLayout(0, 1));
-        pane.add(label1);
-        pane.add(label2);
-        pane.add(label3);
-        pane.add(label4);
-        pane.add(label5);
-        pane.add(label6);
-        pane.add(picLabel);
-        JFrame frame = new JFrame("Team7");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(pane, BorderLayout.CENTER);
-        frame.pack();
-        //Display the window.
-        frame.setVisible(true);
+		try {
+			String filename2 = "version.properties";
+			String filename = "application.properties";
+			input = new FileInputStream(filename);
+			input2 = new FileInputStream(filename2);
+
+			// load a properties file from class path, inside static method
+			application.load(input);
+			version.load(input2);
+
+			// get the property value and print it out
+			teamName = application.getProperty("teamName");
+			teamLeader = application.getProperty("teamLeader");
+			leaderEmail = application.getProperty("leaderEmail");
+			teamMembers = application.getProperty("teamMembers");
+			teamLogo = application.getProperty("teamLogo");
+			ver = version.getProperty("build.number");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (input2 != null) {
+				try {
+					input2.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		JLabel label1 = new JLabel("Team name: " + teamName);
+		JLabel label2 = new JLabel("Leader: " + teamLeader);
+		JLabel label3 = new JLabel("Leader email: " + leaderEmail);
+		JLabel label4 = new JLabel("Team members: " + teamMembers);
+		JLabel label5 = new JLabel("Version number: " + ver);
+		JLabel label6 = new JLabel("Logo: ");
+
+		BufferedImage myPicture = ImageIO.read(new File(teamLogo));
+		JLabel picLabel = new JLabel(new ImageIcon(myPicture),
+				SwingConstants.LEFT);
+
+		JPanel pane = new JPanel();
+		pane.setBorder(BorderFactory.createEmptyBorder(30, // top
+				30, // left
+				10, // bottom
+				30) // right
+		);
+		pane.setLayout(new GridLayout(0, 1));
+		pane.add(label1);
+		pane.add(label2);
+		pane.add(label3);
+		pane.add(label4);
+		pane.add(label5);
+		pane.add(label6);
+		pane.add(picLabel);
+		JFrame frame = new JFrame("Team7");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(pane, BorderLayout.CENTER);
+		frame.pack();
+		// Display the window.
+		frame.setVisible(true);
 	}
 
 }
