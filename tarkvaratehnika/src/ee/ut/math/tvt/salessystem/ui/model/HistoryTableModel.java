b67ea3556c83edf3a572af1c41d6fcc6c1f5cf65
Handre Elias <handreelias@gmail.com>
2014-10-19 15:34:34 +0300

    The History table model

---
+++ tarkvaratehnika/src/ee/ut/math/tvt/salessystem/ui/model/HistoryTableModel.java
@@ -0,0 +1,30 @@
+package ee.ut.math.tvt.salessystem.ui.model;
+
+import ee.ut.math.tvt.salessystem.domain.data.Purchase;
+
+public class HistoryTableModel extends SalesSystemTableModel<Purchase> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public HistoryTableModel() {
+		super(new String[] {"Date", "Time", "Order summary"});
+	}
+
+	@Override
+	protected Object getColumnValue(Purchase item, int columnIndex) {
+		switch (columnIndex) {
+		case 0:
+			return item.getDate();
+		case 1:
+			return item.getTime();
+		case 2:
+			return item.getOrderSum();
+		}
+		throw new IllegalArgumentException("Column index out of range");
+	}
+
+
+}
